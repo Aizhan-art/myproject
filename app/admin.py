ab669c6318555e3eab646f6d617b74adbaa92ef5
Aizhan <osrarova.i@icloud.com>
2025-02-21 21:52:22 +0600

    with car_create & car_detail

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category, Color, Car, News
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Color)
+admin.site.register(Car)
+admin.site.register(News)
--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index_view, name='index'),
-    path('car_create/', views.car_create_view),
+    path('car_create/', views.car_create_view, name='car_create'),
     path('car_detail/<int:pk>/', views.car_detail_view, name='detail'),
     path('news_create/', views.news_create_view, name='news_create'),
     path('car_delete/<int:pk>/', views.car_delete, name='delete'),
